3e7d467126ec4ce9be8deaf20b1ff946f2607b0a
thehellbean <me@hellbean.com>
2020-07-26 10:01:18 +0200

    Refactor getMoves to reduce code duplication

--- com/hellbean/blindfold/Piece.java
+++ com/hellbean/blindfold/Piece.java
@@ -1,5 +1,6 @@
 package com.hellbean.blindfold;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Piece {
@@ -21,6 +22,36 @@ public abstract class Piece {
 
     public void setCurrentSquare(Square currentSquare) {
         this.currentSquare = currentSquare;
+    }
+
+    public List<Square> getMovesFromDeltas(MoveDelta[] moveDeltas, Board board) {
+        int currRank = this.getCurrentSquare().getRank();
+        int currFile = this.getCurrentSquare().getFile();
+
+        List<Square> squares = new ArrayList<>();
+
+        for (int moveIndex = 0; moveIndex < moveDeltas.length; moveIndex++) {
+            for (int i = 1; i < 8; i++) {
+                int newRank = currRank + moveDeltas[moveIndex].getRankOffset(i);
+                int newFile = currFile + moveDeltas[moveIndex].getFileOffset(i);
+                
+                if (newRank < 8 && newRank > 0 && newFile < 8 && newFile > 0) {
+                    Square target = board.getSquare(newRank, newFile);
+                    if (target.getPiece() != null) {
+                        if (target.getPiece().getColor() != getColor()) {
+                            squares.add(target);
+                        }
+                        break;
+                    } else {
+                        squares.add(target);
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return squares;
     }
 }
 
